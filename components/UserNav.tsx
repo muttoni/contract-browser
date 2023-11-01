2ab78df95e8b20ca45656a2fc3788fdcbf7f1f81
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2023-11-01 11:05:03 +0100

    build fixes

--- components/UserNav.tsx
+++ components/UserNav.tsx
@@ -22,8 +22,8 @@ import {
   DropdownMenuSubContent,
 } from "@/components/ui/dropdown-menu"
 
-import { formatFlowBalance } from "@/lib/utils"
-import { useNetworkForAddress, getNetworkConfig } from '@/hooks/useNetwork'
+import { formatFlowBalance, getNetworkFromAddress } from "@/lib/utils"
+import { getNetworkConfig } from '@/hooks/useNetwork'
 import { useCurrentUser } from '@/hooks/useCurrentUser'
 import { useAccount } from '@/hooks/useAccount'
 import ConnectionLight from "./ui/ConnectionLight"
@@ -35,14 +35,13 @@ import {
 } from "./ui/avatar"
 import { useEffect, useState } from "react"
 import { useSearchParams } from "next/navigation"
+import Loading from "./ui/Loading"
 
 
 export function UserNav() {
   
   const user = useCurrentUser()
   const [network, setNetwork] = useState("mainnet")
-  const account = useAccount(user?.addr || "0x11ca36743554b4b0")
-  const accountStorage = account?.storage
   const searchParams = useSearchParams()
   const networkParam = searchParams.get("network")
   
@@ -66,122 +65,140 @@ export function UserNav() {
 
   return (
     <DropdownMenu>
+    {user && !user.loggedIn &&
+      <Button onClick={user.logIn}>
+        <LogIn className="h-4 w-4 me-2" />
+        <span className="hidden md:inline-block">Connect Wallet</span>
+      </Button>
+    }
 
-{!user.loggedIn && 
-  <Button onClick={user.logIn}>
-    <LogIn className="h-4 w-4 me-2" />
-    <span className="hidden md:inline-block">Connect Wallet</span>
-  </Button>
+    {user.loggedIn && (
+      <AccountNav {...user} />
+    )}
+    </DropdownMenu>
+  )
 }
-{user.loggedIn && (
+
+
+export default function AccountNav(user) {
+  const account = useAccount(user.addr)
+  const network = getNetworkFromAddress(user.addr)
+
+  return user && account ? (
+    <>
+    <DropdownMenuTrigger asChild>
+    <Button variant="outline">
+      <ConnectionLight status="online"/><span className="w-2"></span> 
+      <span className="hidden md:inline-block">{ account.storage && account.storage?.find ? account.storage?.find.name : user?.addr }</span>
+      {account.storage && account.storage?.find ? 
+      <Avatar className="h-6 w-6 me-2 md:hidden">
+        <AvatarImage src={account.storage?.find.avatar} alt={account.storage?.find.name} />
+      </Avatar>
+      : <UserCircle className="h-4 w-4 md:hidden" />
+      }
+      <ChevronDown className="h-4 w-4 ml-1" />
+    </Button>
+  </DropdownMenuTrigger>
+    <DropdownMenuContent className="w-56" align="end" forceMount>
+      <DropdownMenuLabel className="font-normal">
+        <div className="flex flex-col space-y-1">
+          <p className="text-sm font-medium leading-none">You are logged in as</p>
+          <p className="text-xs leading-none text-muted-foreground">
+            {user?.addr}
+          </p>
+
+          {account.storage && account.storage?.find && 
+          <>
+            <div className="flex pt-2 pb-1 items-center">
+            <Avatar className="h-6 w-6 me-2">
+              <AvatarImage src={account.storage?.find.avatar} alt={account.storage?.find.name} />
+              <AvatarFallback>{account.storage?.find.name[0]}</AvatarFallback>
+            </Avatar>
+            <Link href={{pathname: `https://find.xyz/${account.storage?.find.name}`}} target="_blank"> {account.storage?.find.name}.find  </Link>
+            </div>
+            <div>
+              <p className="text-xs text-muted-foreground">{account.storage?.find.description}  </p>
+            </div>
+          </>
+          }
+        </div>
+      </DropdownMenuLabel>
+        <Link href={"/account/" + user?.addr + "/"}>
+        <DropdownMenuItem>
+        <div className="flex-row">
+          <div>Your account</div>
+          <div className="text-xs flex space-x-1 align-middleleading-none text-muted-foreground">
+            <FlowLogo size={16} />
+            <span>{ formatFlowBalance(account?.flowBalance) }</span></div>
+          </div>
+          <DropdownMenuShortcut>
+            <Eye className="h-4 w-4" />
+          </DropdownMenuShortcut>
+        </DropdownMenuItem>
+        </Link>
+      <Link href={"/account/" + user?.addr + "/contracts"}>
+      <DropdownMenuItem>
+        Manage contracts
+        <DropdownMenuShortcut>
+        { Object.keys(account?.contractNames || {})?.length ?? 0 }
+          </DropdownMenuShortcut>
+      </DropdownMenuItem>
+      </Link>
+      <Link href={"/account/" + user?.addr + "/contracts/deploy"}>
+      <DropdownMenuItem>
+        Deploy a new contract
+        <DropdownMenuShortcut>
+          <Plus className="h-4 w-4" />
+        </DropdownMenuShortcut>
+      </DropdownMenuItem>
+      </Link>
+      <DropdownMenuSeparator />
+
+      <DropdownMenuSub>
+        <DropdownMenuSubTrigger><span className="text-muted-foreground me-2">Network:</span><span className="capitalize">{network}</span></DropdownMenuSubTrigger>
+        <DropdownMenuPortal>
+          <DropdownMenuSubContent>
+          <DropdownMenuRadioGroup value={network}>
+            <DropdownMenuRadioItem value={"mainnet"} >Mainnet</DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value={"testnet"}>Testnet</DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+          </DropdownMenuSubContent>
+        </DropdownMenuPortal>
+      </DropdownMenuSub>
+
+      <DropdownMenuSeparator />
+    
+      <DropdownMenuGroup>
+        <DropdownMenuItem>
+          Settings    
+          <DropdownMenuShortcut>
+          <Cog className="h-4 w-4 text-muted-foreground" />
+          </DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuGroup>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem onClick={user.changeUser}>
+        Switch Account
+        <DropdownMenuShortcut>
+        <ArrowLeftRight className="h-4 w-4 text-muted-foreground" />
+        </DropdownMenuShortcut>
+      </DropdownMenuItem>
+      <DropdownMenuItem onClick={user.logOut}>
+        Log out
+        <DropdownMenuShortcut>
+        <LogOut className="h-4 w-4 text-muted-foreground" />
+        </DropdownMenuShortcut>
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+    </>
+  ) : (
     <>
     <DropdownMenuTrigger asChild>
       <Button variant="outline">
         <ConnectionLight status="online"/><span className="w-2"></span> 
-        <span className="hidden md:inline-block">{ accountStorage && accountStorage?.find ? accountStorage?.find.name : user?.addr }</span>
-        {accountStorage && accountStorage?.find ? 
-        <Avatar className="h-6 w-6 me-2 md:hidden">
-          <AvatarImage src={accountStorage?.find.avatar} alt={accountStorage?.find.name} />
-        </Avatar>
-        : <UserCircle className="h-4 w-4 md:hidden" />
-        }
-        <ChevronDown className="h-4 w-4 ml-1" />
+        <span className="hidden md:inline-block"><Loading className="h-5 w-5 m-0"/></span>
       </Button>
     </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">You are logged in as</p>
-            <p className="text-xs leading-none text-muted-foreground">
-              {user?.addr}
-            </p>
-
-            {accountStorage && accountStorage?.find && 
-            <>
-              <div className="flex pt-2 pb-1 items-center">
-              <Avatar className="h-6 w-6 me-2">
-                <AvatarImage src={accountStorage?.find.avatar} alt={accountStorage?.find.name} />
-                <AvatarFallback>{accountStorage?.find.name[0]}</AvatarFallback>
-              </Avatar>
-              <Link href={{pathname: `https://find.xyz/${accountStorage?.find.name}`}} target="_blank"> {accountStorage?.find.name}.find  </Link>
-              </div>
-              <div>
-                <p className="text-xs text-muted-foreground">{accountStorage?.find.description}  </p>
-              </div>
-            </>
-            }
-          </div>
-        </DropdownMenuLabel>
-          <Link href={"/account/" + user?.addr + "/"}>
-          <DropdownMenuItem>
-          <div className="flex-row">
-            <div>Your account</div>
-            <div className="text-xs flex space-x-1 align-middleleading-none text-muted-foreground">
-              <FlowLogo size={16} />
-              <span>{ formatFlowBalance(account?.flowBalance) }</span></div>
-            </div>
-            <DropdownMenuShortcut>
-              <Eye className="h-4 w-4" />
-            </DropdownMenuShortcut>
-          </DropdownMenuItem>
-          </Link>
-        <Link href={"/account/" + user?.addr + "/contracts"}>
-        <DropdownMenuItem>
-          Manage contracts
-          <DropdownMenuShortcut>
-          { Object.keys(account?.contractNames || {})?.length ?? 0 }
-            </DropdownMenuShortcut>
-        </DropdownMenuItem>
-        </Link>
-        <Link href={"/account/" + user?.addr + "/contracts/deploy"}>
-        <DropdownMenuItem>
-          Deploy a new contract
-          <DropdownMenuShortcut>
-            <Plus className="h-4 w-4" />
-          </DropdownMenuShortcut>
-        </DropdownMenuItem>
-        </Link>
-        <DropdownMenuSeparator />
-
-        <DropdownMenuSub>
-          <DropdownMenuSubTrigger><span className="text-muted-foreground me-2">Network:</span><span className="capitalize">{network}</span></DropdownMenuSubTrigger>
-          <DropdownMenuPortal>
-            <DropdownMenuSubContent>
-            <DropdownMenuRadioGroup value={network} onValueChange={changeNetwork}>
-              <DropdownMenuRadioItem value={"mainnet"} >Mainnet</DropdownMenuRadioItem>
-              <DropdownMenuRadioItem value={"testnet"}>Testnet</DropdownMenuRadioItem>
-            </DropdownMenuRadioGroup>
-            </DropdownMenuSubContent>
-          </DropdownMenuPortal>
-        </DropdownMenuSub>
-
-        <DropdownMenuSeparator />
-      
-        <DropdownMenuGroup>
-          <DropdownMenuItem>
-            Settings    
-            <DropdownMenuShortcut>
-            <Cog className="h-4 w-4 text-muted-foreground" />
-            </DropdownMenuShortcut>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={user.changeUser}>
-          Switch Account
-          <DropdownMenuShortcut>
-          <ArrowLeftRight className="h-4 w-4 text-muted-foreground" />
-          </DropdownMenuShortcut>
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={user.logOut}>
-          Log out
-          <DropdownMenuShortcut>
-          <LogOut className="h-4 w-4 text-muted-foreground" />
-          </DropdownMenuShortcut>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-      </>
-)}
-    </DropdownMenu>
-  
+    </>
   )
 }
