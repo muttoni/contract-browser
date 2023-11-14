5aef8c652778640d45be9ec3bce0a1dc4e37b155
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2023-11-14 16:43:51 +0100

    metadata fixes

--- app/(platform)/[contractId]/layout.tsx
+++ app/(platform)/[contractId]/layout.tsx
@@ -1,94 +1,25 @@
-"use client"
-import { SidebarNav } from "@/components/AccountSideBar"
-import { useParams } from "next/navigation"
-import { useNetworkForAddress } from "@/hooks/useNetwork"
-import { useCurrentUser } from "@/hooks/useCurrentUser"
-import { getContractAddress, getContractName, cn } from "@/lib/utils"
-import { Badge } from "@/components/ui/badge"
-import { Separator } from "@/components/ui/separator"
-import Link from "next/link"
-import AddressBadge from "@/components/ui/AddressBadge"
-import { CaretRightIcon } from "@radix-ui/react-icons"
-import { BadgeHelp } from "lucide-react"
-import { isVerified } from "@/lib/verified-contracts"
-import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card"
-import { VerifiedBadge } from "@/components/ui/VerifiedBadge"
+import ContractPageLayout from "@/components/pages/ContractPageLayout";
 
-export default function ContractLayout({ children }) {
-  
-  const { contractId } = useParams()
-  const user = useCurrentUser()
-  const network = useNetworkForAddress(getContractAddress(contractId))
+import type { Metadata, ResolvingMetadata} from 'next'
+import { generateMetadataObject, getContractAddress, getContractName, getNetworkFromAddress } from "@/lib/utils"
 
-  const sidebarNavItems = [
-    {
-      title: "Overview",
-      type: "parent",
-      href: "/" + contractId,
-    },
-    {
-      title: "Snippets",
-      type: "parent",
-      href: "/" + contractId + "/snippets",
-    },
-    {
-      title: "Used by",
-      type: "parent",
-      href: "/" + contractId + "/dependants",
-    },
-    {
-      title: "History",
-      type: "parent",
-      href:"/" + contractId + "/events",
-    }
-  ]
-
-  return (
-    <div className="space-y-6 pt-4 pb-16 flex flex-col flex-1">
-      <div className="space-y-2">
-        <h2 className="text-3xl items-center flex gap-2 font-bold tracking-tight">
-          <span className="">{getContractName(contractId)}</span>
-          {isVerified(contractId as string) ? <VerifiedBadge size={6}/> : <NotVerifiedBadge />}
-        </h2>
-        <div className="flex items-center">
-          <Badge className={cn("rounded-sm h-6 text-xs font-mono font-light uppercase", `${network === 'testnet' ? "border-orange-600 bg-orange-400 hover:bg-orange-400 text-orange-800" : "border-green-600 bg-green-400 hover:bg-green-400 text-green-800"}`)}>{network}</Badge>
-          <CaretRightIcon className="h-6 w-6 mx-0 text-muted-foreground" />
-          <AddressBadge className="text-sm h-6" address={getContractAddress(contractId)} copyBadge={true}/>
-
-          {/* <p className="text-xs">
-            getContractAddress(contractId) === user?.addr ? 
-            "You own this contract, so you can update it." : 
-            `Login with the deploying account to modify this contract.`
-          </p> */ }
-
-        </div>
-      </div>
-      <Separator className="my-6" />
-      <div className="flex flex-col flex-1 space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-        <aside className="">
-          <SidebarNav items={sidebarNavItems} />
-        </aside>
-        <div className="flex-1 space-y-4 lg:ps-4">
-        {children}
-        </div>
-      </div>
-    </div>
-  )
+type Props = {
+  params: { contractId: string }
 }
 
-const NotVerifiedBadge = () => {
-  return (
-    <HoverCard>
-      <HoverCardTrigger>
-        <BadgeHelp className="h-6 w-6 text-muted" />
-      </HoverCardTrigger>
-      <HoverCardContent className="flex flex-col space-y-2 font-sans text-sm text-start tracking-normal">
-        <div className="text-sm font-bold">This contract is not verified.</div>
-        <div className="text-sm text-muted-foreground font-normal">Be mindful you are sourcing from the correct account.</div>
-        <Link target="_blank" href="https://github.com/muttoni/contract-browser-new/blob/main/lib/official-contracts.ts" className="text-sm text-blue-600 hover:underline">
-          Verify your contract
-        </Link>
-      </HoverCardContent>
-    </HoverCard>
-  )
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const contractId = params.contractId as string
+  const contractName = getContractName(contractId)
+  const contractAddress = getContractAddress(contractId)
+  const network = getNetworkFromAddress(contractAddress)
+
+  return generateMetadataObject(contractName + " deployed by " + contractAddress + " on " + network, contractName + " is a contract deployed on the Flow blockchain")
+}
+
+export default function ContractLayout({ children }) {
+  return <ContractPageLayout>{children}</ContractPageLayout>
 }
---
+++ components/pages/AccountPageLayout.tsx
@@ -0,0 +1,81 @@
+"use client"
+import { useCurrentUser } from "@/hooks/useCurrentUser"
+import { Separator } from "@/components/ui/separator"
+import { SidebarNav } from "@/components/AccountSideBar"
+import { useParams } from "next/navigation"
+import { useNetworkForAddress } from "@/hooks/useNetwork"
+import { Badge } from "@/components/ui/badge"
+import { cleanAddress, cn } from "@/lib/utils"
+
+export default function AccountPageLayout({ children }) {
+
+  const user = useCurrentUser()
+  const params = useParams()
+  const address = cleanAddress(params.address as string)
+  const network = useNetworkForAddress(address)
+
+  const sidebarNavItems = [
+    {
+      title: "Overview",
+      type: "parent",
+      href: "/account/" + address,
+    },
+    {
+      title: "Contracts",
+      type: "parent",
+      href: "/account/" + address + "/contracts",
+    },
+
+    {
+      title: "Deploy",
+      type: "sub",
+      href: "/account/" + address + "/contracts/deploy",
+    },
+    // {
+    //   title: "Tokens",
+    //   type: "parent",
+    //   href: "/account/" + params.address + "/tokens",
+    // },
+    // {
+    //   title: "Storage",
+    //   type: "parent",
+    //   href: "/account/" + params.address + "/storage",
+    // },
+    // {
+    //   title: "Keys",
+    //   type: "parent",
+    //   href: "/account/" + params.address + "/keys",
+    // },
+  ]
+
+  return (
+
+    <div className="space-y-6 pt-4 pb-16 h-full flex-1">
+      <div className="space-y-0.5">
+        <h2 className="text-2xl items-center flex gap-2 font-bold tracking-tight">
+
+          Account <span className="text-muted-foreground">{address}</span>
+          <Badge className={cn("rounded-sm h-6 font-mono font-light uppercase", `${network === 'testnet' ? "border-orange-600 bg-orange-400 hover:bg-orange-400 text-orange-800" : "border-green-600 bg-green-400 hover:bg-green-400 text-green-800"}`)}>{network}</Badge>
+          {address === user?.addr &&
+            <Badge className={cn("rounded-sm h-6 font-mono font-light uppercase whitespace-nowrap","border-blue-600 bg-blue-400 hover:bg-blue-400 text-blue-800")}>Current User</Badge>
+          }
+        </h2>
+        <p className="text-muted-foreground">
+          {
+            address === user?.addr ? 
+            "Inspect and manage your account, contracts and keys." : 
+            `Viewing the account of ${address}. Login with this account to access all features.`
+          }
+        </p>
+      </div>
+      <Separator className="my-6" />
+      <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+        <aside className="mx-4 lg:mx-0 lg:w-1/5">
+          <SidebarNav items={sidebarNavItems} />
+        </aside>
+        <div className="flex-1">{children}</div>
+      </div>
+    </div>
+
+  )
+}
