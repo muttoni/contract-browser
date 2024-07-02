f11bb6a69f14f74d7fdd4ec4ebf0ed0ac77a95b3
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2024-07-02 15:42:55 +0200

    cache

--- app/(api)/api/crescendo/staged/testnet/route.ts
+++ app/(api)/api/crescendo/staged/testnet/route.ts
@@ -10,5 +10,11 @@ export async function GET() {
   const res = await fetch(`${process.env.DNZ_STAGE_URL}`, options)
   const response : DNZResponse = await res.json()
 
-  return Response.json(response)
+  return Response.json(response, {
+    headers: {
+      'Cache-Control': 'public, s-maxage=500',
+      'CDN-Cache-Control': 'public, s-maxage=500',
+      'Vercel-CDN-Cache-Control': 'public, s-maxage=500',
+    },
+  })
 }
