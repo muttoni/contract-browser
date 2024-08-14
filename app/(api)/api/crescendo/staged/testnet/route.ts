0c48da15139395671dda57a137daaa6362298e5c
Andrea Muttoni <maverickmuttoni+github@gmail.com>
2024-08-14 19:23:18 +0200

    general imporvements

--- app/(api)/api/crescendo/staged/testnet/route.ts
+++ app/(api)/api/crescendo/staged/testnet/route.ts
@@ -8,6 +8,7 @@ export async function GET() {
   };
 
   const res = await fetch(`${process.env.DNZ_STAGE_URL}`, options)
+  if(!res.ok) return;
   const response : DNZResponse = await res.json()
 
   return Response.json(response)
