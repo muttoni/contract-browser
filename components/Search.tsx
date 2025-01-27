9de404171f55e1209c5eb1e6983cce9de04200b1
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2025-01-27 17:12:51 +0100

    Update Search.tsx to direct to evm.flowscan.io instead of flowscan.io
    Update Search.tsx to direct to evm.flowscan.io instead of flowscan.io for EVM Mainnet Explorer

--- components/Search.tsx
+++ components/Search.tsx
@@ -325,7 +325,7 @@ export function Search() {
               <li>
                 Mainnet:{" "}
                 <a
-                  href="https://www.flowscan.io/"
+                  href="https://www.evm.flowscan.io/"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="text-blue-500 dark:text-blue-400 hover:underline"
